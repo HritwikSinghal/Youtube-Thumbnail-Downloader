192609ca386b95c93c8d97245186cf01326f9a28
HritwikSinghal <29531474+HritwikSinghal@users.noreply.github.com>
2018-05-29 00:12:43 +0530

    Update Thumbnail_download.py

--- Thumbnail_download.py
+++ Thumbnail_download.py
@@ -1,7 +1,7 @@
 # youtube videos thumbnail downloader and resizer and alse cropper (all at once)
 # created by Hritwik Singhal on 28-05-2018: 03:20
 
-# i am a newbie to python(and programming), so you can assume that this piece is 
+# i am a newbie to python(and programming), so you can assume that this piece is
 # poorly written, poorly commented etc etc
 # Also, you can modify functions as per your need.
 
@@ -10,9 +10,8 @@ from PIL import Image
 
 
 # you should have a list of ID of videos,separated by new line,
-# in a file called 'ids' with no file extension 
+# in a file called 'ids' with no file extension
 def download_image(ids):
-
     ID_list = []
     for x in open(ids).read().split():
         ID_list.append(x.strip())
@@ -32,12 +31,12 @@ def download_image(ids):
 
         img_open = Image.open(name)
 
-		# the image is 1280* 720
+        # the image is 1280* 720
         # resizing the image
         resize_img = img_open.resize((900, 600))
 
         # Cropping image
-		# This crops the middle 500*500 portion of image
+        # This crops the middle 500*500 portion of image
         crop_area = (200, 50, 700, 570)
         cropped_img = resize_img.crop(crop_area)
         cropped_img.save(name)
