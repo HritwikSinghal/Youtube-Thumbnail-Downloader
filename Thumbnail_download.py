2b03d9af56ee8a57b70198ea30ae21b3dfd9ae8c
Hritwik <hritwiksinghal@outlook.in>
2020-07-17 18:52:43 +0530

    img name made video title, rewrite

--- Thumbnail_download.py
+++ Thumbnail_download.py
@@ -5,43 +5,77 @@
 # poorly written, poorly commented etc etc
 # Also, you can modify functions as per your need.
 
-import urllib
-
-from PIL import Image
-
-
 # you should have a list of ID of videos,separated by new line,
 # in a file called 'ids' with no file extension
-def download_image(ids):
-    ID_list = []
-    for x in open(ids).read().split():
-        y = x.strip()
-        ID_list.append(y[32:43])
 
-    # take ID of video
-    for one_ID in ID_list:
-        ID = one_ID
+import os
+import re
 
-        # insert ID in url of image
-        url = 'https://img.youtube.com/vi/' + ID + '/maxresdefault.jpg'
-
-        # Naming of images
-        name = ID + '.jpg'
-
-        # downloading the image
-        urllib.urlretrieve(url, name)
-
-        img_open = Image.open(name)
-
-        # the image is 1280* 720
-        # resizing the image
-        resize_img = img_open.resize((900, 600))
-
-        # Cropping image
-        # This crops the middle 500*500 portion of image
-        crop_area = (200, 50, 700, 570)
-        cropped_img = resize_img.crop(crop_area)
-        cropped_img.save(name)
+import requests
+from PIL import Image
+from bs4 import BeautifulSoup
 
 
-download_image('ID.txt')
+def download_image(ID, url):
+    # insert ID in url of image
+    img_url = 'https://img.youtube.com/vi/' + ID + '/maxresdefault.jpg'
+
+    # Naming of images
+    res = requests.get(url)
+    soup = BeautifulSoup(res.text, 'html.parser')
+    title = str(soup.title)
+    title = re.findall(r'<title>(.*)- YouTube</title>', title)[0].strip()
+    name = title + '.jpg'
+
+    # downloading the image
+    print("Downloading '{0}'.....".format(title))
+    raw_data = requests.get(img_url, stream=True)
+    with open(name, "wb") as raw_img:
+        for chunk in raw_data.iter_content(chunk_size=2048):
+            if chunk:
+                raw_img.write(chunk)
+    print("Download Successful")
+
+    img = Image.open(name)
+
+    # the image is 1280* 720
+    # resizing the image
+    img = img.resize((900, 600))
+
+    # Cropping image
+    # This crops the middle 500*500 portion of image
+    crop_area = (200, 50, 700, 570)
+    img = img.crop(crop_area)
+    img.save(name)
+
+
+def start():
+    os.chdir(os.getcwd())
+
+    if os.path.isfile('ID.txt'):
+        for line in open('ID.txt').read().split("\n"):
+            if line.strip().startswith('https://www.youtube.com/'):
+                url = line
+                video_id = url.strip()[32:43]
+                try:
+                    download_image(video_id, url)
+                except:
+                    print("There was some Error")
+
+    else:
+        print("Couldn't find 'ID.txt' file.")
+
+        while True:
+            url = input("Enter Youtube URL.\n")
+
+            if url.strip().startswith('https://www.youtube.com/'):
+                video_id = url.strip()[32:43]
+                try:
+                    download_image(video_id, url)
+                except:
+                    print("There was some Error")
+            else:
+                print("Wrong URL entered. Enter url in this format: 'https://www.youtube.com/watch?v=EOfCEhWq420'")
+
+
+start()
